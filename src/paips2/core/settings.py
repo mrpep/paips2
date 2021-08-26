a65ec619523b2ddc45066c9c869e29e04d537983
mrpep <leonardodpepino@gmail.com>
2021-08-26 02:40:02 -0300

    update common params

--- src/paips2/core/settings.py
+++ src/paips2/core/settings.py
@@ -1,4 +1,4 @@
 symbols = dict(membership = '->')
 not_cacheable_params = ['cache', 'export']
-common_optional_params = ['cache', 'export', 'in_memory', 'priority', 'in']
+common_optional_params = ['cache', 'export', 'in_memory', 'priority', 'in', 'lazy']
 common_required_params = ['class']
