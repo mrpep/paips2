e9e5bcb0137ad9b61ce3a827997eb219cbde6262
mrpep <leonardodpepino@gmail.com>
2021-09-13 00:44:35 -0300

    pedalboard added to pipeline

--- src/paips2/tasks/audio/__init__.py
+++ src/paips2/tasks/audio/__init__.py
@@ -1,6 +1,9 @@
 from paips2.core import Task
 import soundfile as sf
 import numpy as np
+import pedalboard
+from paips2.utils import get_classes_in_module
+from pathlib import Path
 
 class ReadAudio(Task):
     def get_valid_parameters(self):
@@ -9,3 +12,19 @@ class ReadAudio(Task):
     def process(self):
         x,fs = sf.read(self.config['in'])
         return x
+
+class Pedalboard(Task):
+    def get_valid_parameters(self):
+        return ['in', 'pedals'], ['sr']
+    
+    def process(self):
+        wav = self.config['in']
+        sr = self.config.get('sr',44100)
+        available_effects = get_classes_in_module(pedalboard)
+        pedals_config = self.config['pedals']
+        pedals_config = [{list(p.keys())[0]: {k: v if k != 'mode' else getattr(available_effects[list(p.keys())[0]].Mode, v) for k,v in p[list(p.keys())[0]].items()}} for p in pedals_config]
+        effects_pedalboard = pedalboard.Pedalboard([available_effects[list(p.keys())[0]](**p[list(p.keys())[0]]) for p in pedals_config], sample_rate=sr)
+        return effects_pedalboard(wav)
+
+    def on_export(self,outs):
+        sf.write(str(Path(self.export_path,'out.wav').absolute()),outs[self.name + '->out'].load(),samplerate=self.config.get('sr',44100))
