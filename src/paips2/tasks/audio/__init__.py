59a93ad56a2b4d30b9dae7a62ef1f5f7a4cc2ecf
mrpep <leonardodpepino@gmail.com>
2021-10-07 22:24:21 -0300

    pedalboard probabilities

--- src/paips2/tasks/audio/__init__.py
+++ src/paips2/tasks/audio/__init__.py
@@ -21,16 +21,21 @@ class ReadAudio(Task):
 
 class Pedalboard(Task):
     def get_valid_parameters(self):
-        return ['in', 'pedals'], ['sr']
+        return ['in', 'pedals'], ['sr','probability']
     
     def process(self):
+        p = self.config.get('probability',1.0)
         wav = self.config['in']
         sr = self.config.get('sr',44100)
-        available_effects = get_classes_in_module(pedalboard)
-        pedals_config = self.config['pedals']
-        pedals_config = [{list(p.keys())[0]: {k: v if k != 'mode' else getattr(available_effects[list(p.keys())[0]].Mode, v) for k,v in p[list(p.keys())[0]].items()}} for p in pedals_config]
-        effects_pedalboard = pedalboard.Pedalboard([available_effects[list(p.keys())[0]](**p[list(p.keys())[0]]) for p in pedals_config], sample_rate=sr)
-        return effects_pedalboard(wav)
+        p_ = np.random.uniform(low=0,high=1)
+        if p_ <= p:
+            available_effects = get_classes_in_module(pedalboard)
+            pedals_config = self.config['pedals']
+            pedals_config = [{list(p.keys())[0]: {k: v if k != 'mode' else getattr(available_effects[list(p.keys())[0]].Mode, v) for k,v in p[list(p.keys())[0]].items()}} for p in pedals_config]
+            effects_pedalboard = pedalboard.Pedalboard([available_effects[list(p.keys())[0]](**p[list(p.keys())[0]]) for p in pedals_config], sample_rate=sr)
+            return effects_pedalboard(wav)
+        else:
+            return wav
 
     def on_export(self,outs):
         sf.write(str(Path(self.export_path,'out.wav').absolute()),outs[self.name + '->out'].load(),samplerate=self.config.get('sr',44100))
