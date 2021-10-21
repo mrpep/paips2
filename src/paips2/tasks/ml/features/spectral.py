cf7922ee3dc985f51bfbe82467b52a654922df1c
mrpep94 <leonardodpepino@gmail.com>
2021-10-21 16:27:44 -0300

    return time frequency representations with shape [TxF] instead of [FxT]

--- src/paips2/tasks/ml/features/spectral.py
+++ src/paips2/tasks/ml/features/spectral.py
@@ -6,6 +6,12 @@ class TimeFrequencyRepresentation(Task):
     def get_valid_parameters(self):
         return ['in'], ['representation', 'log', 'log_offset', 'parameters']
     
+    def get_output_names(self):
+        if self.config.get('return_len') is not None:
+            return ['out','len']
+        else:
+            return ['out']
+
     def process(self):
         tfr = self.config.get('representation','melspectrogram')
         apply_log = self.config.get('log',False)
@@ -13,7 +19,7 @@ class TimeFrequencyRepresentation(Task):
         tfr_params = self.config.get('parameters',{})
         x = self.config.get('in',None)
         if tfr == 'melspectrogram':
-            y = librosa.feature.melspectrogram(x,**tfr_params)
+            y = librosa.feature.melspectrogram(x,**tfr_params).T
         if apply_log:
             y = np.log(y + log_offset)
         return y
