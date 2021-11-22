833dc7f1820863b2353aaa270579876defbdfa6f
mrpep94 <leonardodpepino@gmail.com>
2021-11-22 17:29:57 -0300

    add possibility of adding deltas in time frequency representations

--- src/paips2/tasks/ml/features/spectral.py
+++ src/paips2/tasks/ml/features/spectral.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class TimeFrequencyRepresentation(Task):
     def get_valid_parameters(self):
-        return ['in'], ['representation', 'log', 'log_offset', 'parameters']
+        return ['in'], ['representation', 'log', 'log_offset', 'parameters', 'delta', 'delta_delta']
     
     def get_output_names(self):
         if self.config.get('return_len') is not None:
@@ -19,8 +19,21 @@ class TimeFrequencyRepresentation(Task):
         tfr_params = self.config.get('parameters',{})
         x = self.config.get('in',None)
         if tfr == 'melspectrogram':
-            y = librosa.feature.melspectrogram(x,**tfr_params).T
+            y = librosa.feature.melspectrogram(x,**tfr_params)
         if apply_log:
             y = np.log(y + log_offset)
+        delta = self.config.get('delta',False)
+        delta_delta = self.config.get('delta_delta',False)
+        if delta:
+            y_delta = librosa.feature.delta(y)
+        if delta_delta:
+            y_delta_delta = librosa.feature.delta(y,order=2)
+        if delta:
+            y = np.concatenate((y,y_delta),axis=-2)
+        if delta_delta:
+            y = np.concatenate((y,y_delta_delta),axis=-2)
+
+        y = y.T
+
         return y
 
