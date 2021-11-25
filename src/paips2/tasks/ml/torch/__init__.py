6400e49bfd07845cb2b57633227cf4468dedb4a8
mrpep94 <leonardodpepino@gmail.com>
2021-11-24 23:04:14 -0300

    add swa task

--- src/paips2/tasks/ml/torch/__init__.py
+++ src/paips2/tasks/ml/torch/__init__.py
@@ -1,3 +1,32 @@
 from .torch_generator import TorchGenerator
 from .torch_trainer import TorchTrainer
 from .torch_predictor import TorchPredictor
+
+from paips2.core import Task
+from pathlib import Path
+import torch
+import re
+
+class TorchSWAModel(Task):
+    def get_output_names(self):
+        return ['out']
+
+    def get_valid_parameters(self):
+        return ['checkpoint_dir'], ['n_checkpoints','from_epoch','to_epoch']
+
+    def process(self):
+        available_ckpts = [str(ckpt) for ckpt in Path(self.config['checkpoint_dir']).glob('*.ckpt')]
+        #order checkpoints by epoch
+        available_ckpts = sorted(available_ckpts, key=lambda x: int(re.search(r'epoch=(\d+)', x).group(1)))
+        last_ckpts = available_ckpts[-self.config['n_checkpoints']:]
+
+        swa_weights = {}
+        for ckpt in last_ckpts:
+            weights = torch.load(ckpt)['state_dict']
+            for k,v in weights.items():
+                if k not in swa_weights:
+                    swa_weights[k] = v/len(last_ckpts)
+                else:
+                    swa_weights[k] += v/len(last_ckpts)
+        
+        return swa_weights
