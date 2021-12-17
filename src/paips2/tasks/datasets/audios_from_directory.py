99b0cde7cf5237ad811d6aaeab0321c10e7d5d9f
mrpep94 <leonardodpepino@gmail.com>
2021-12-16 23:09:26 -0300

    extract_audio_metadata as class method

--- src/paips2/tasks/datasets/audios_from_directory.py
+++ src/paips2/tasks/datasets/audios_from_directory.py
@@ -12,6 +12,39 @@ class AudioDatasetFromDirectory(Task):
     def get_valid_parameters(self):
         return ['dataset_path'], ['max_rows', 're', 'audio_extensions', 'split_lists','split_column_in','split_column_out','constant_column']
 
+    def extract_audio_metadata(self, f, dataset_path):
+        #Extract audio info:
+        if f.suffix == '.wav':
+            audio_metadata = sf.info(str(f.absolute())).__dict__
+            audio_metadata['absolute_path'] = audio_metadata.pop('name')
+            audio_metadata.pop('extra_info')
+            audio_metadata.pop('verbose')
+        else:
+            info = pymediainfo.MediaInfo.parse(f)
+            audio_metadata = info.to_data()['tracks']
+            for t in audio_metadata:
+                if t['track_type'] == 'Audio':
+                    audio_metadata = t
+                    break
+            audio_metadata['absolute_path'] = str(f.absolute())
+            audio_metadata['frames'] = audio_metadata.pop('samples_count')
+            for k,v in audio_metadata.items():
+                if isinstance(v,list) and len(v) == 1:
+                    audio_metadata[k] = v[0]
+                elif isinstance(v,list) and len(v) > 1:
+                    audio_metadata[k] = ','.join(v)
+        audio_metadata['relative_path'] = str(f.relative_to(dataset_path))
+        audio_metadata['name'] = str(f.stem)
+        #Match regular expressions:
+        filename_re = self.config.get('re', None)
+        if filename_re:
+            import re
+            re_match = re.match(filename_re,audio_metadata['relative_path'])
+            if re_match is not None:
+                fields = re_match.groupdict()
+                audio_metadata.update(fields)
+        return audio_metadata
+
     def process(self):
         #Gather all audios in directory:
         dataset_path = Path(self.config.get('dataset_path')).expanduser()
@@ -23,37 +56,7 @@ class AudioDatasetFromDirectory(Task):
             available_audios.extend(list(dataset_path.rglob('*.{}'.format(ext))))
         metadatas = []
         for f in tqdm(available_audios):
-            #Extract audio info:
-            if f.suffix == '.wav':
-                audio_metadata = sf.info(str(f.absolute())).__dict__
-                audio_metadata['absolute_path'] = audio_metadata.pop('name')
-                audio_metadata.pop('extra_info')
-                audio_metadata.pop('verbose')
-            else:
-                info = pymediainfo.MediaInfo.parse(f)
-                audio_metadata = info.to_data()['tracks']
-                for t in audio_metadata:
-                    if t['track_type'] == 'Audio':
-                        audio_metadata = t
-                        break
-                audio_metadata['absolute_path'] = str(f.absolute())
-                audio_metadata['frames'] = audio_metadata.pop('samples_count')
-                for k,v in audio_metadata.items():
-                    if isinstance(v,list) and len(v) == 1:
-                        audio_metadata[k] = v[0]
-                    elif isinstance(v,list) and len(v) > 1:
-                        audio_metadata[k] = ','.join(v)
-            audio_metadata['relative_path'] = str(f.relative_to(dataset_path))
-            audio_metadata['name'] = str(f.stem)
-            #Match regular expressions:
-            filename_re = self.config.get('re', None)
-            if filename_re:
-                import re
-                re_match = re.match(filename_re,audio_metadata['relative_path'])
-                if re_match is not None:
-                    fields = re_match.groupdict()
-                    audio_metadata.update(fields)
-            
+            audio_metadata = self.extract_audio_metadata(f,dataset_path)
             metadatas.append(audio_metadata)
 
         #Make dataframe and see if split lists were given:
