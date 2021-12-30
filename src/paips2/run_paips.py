c03a2acdb8f7fa71fe14126eb9fbcaacc0b8b294
mrpep94 <leonardodpepino@gmail.com>
2021-12-30 01:19:03 -0300

    support explorations and save logs

--- src/paips2/run_paips.py
+++ src/paips2/run_paips.py
@@ -7,6 +7,7 @@ from kahnfigh import Config
 from kahnfigh.utils import IgnorableTag
 from paips2.core.compose import process_config
 from paips2.core.settings import ignorable_tags
+from pathlib import Path
 
 import warnings
 import numpy as np
@@ -28,19 +29,36 @@ def main():
         
     format_logger(logger, level=logging_level)
 
-    logger.info('Using backend {}'.format(args['backend']))
-    init_backend(args['backend'])
-    logger.info('Running experiment {}. Outputs will be saved in {}'.format(args['experiment_name'], args['experiment_path']))
-    #config = Config(args['config_path'])
     config = Config(args['config_path'],
               yaml_tags=[IgnorableTag('!{}'.format(tag)) for tag in ignorable_tags])
     config['class'] = 'Graph'
     config, global_config, default_config = process_config(config, mods=args['mods'],logger=logger)
     args['task_modules'] = config.get('task_modules',[])
-
-    graph = Graph(config=config, name='main_graph',logger=logger, global_flags=args, main=True)
-    graph.cacheable = False
-    graph.run()
+    if args['explore'] is not None:
+        from paips2.utils import module_from_file
+        explore_module = module_from_file('exploration_script',args['explore'])
+        modded_configs, modded_args = explore_module.get_configs(config,args)
+        for i,(c,a) in enumerate(zip(modded_configs, modded_args)):
+            if not Path(a['experiment_path']).exists():
+                Path(a['experiment_path']).mkdir(parents=True)
+            sink_id = logger.add(Path(a['experiment_path'],'logs'))
+            logger.info('Running exploration. Experiment {}/{}: {}. Outputs will be saved in {}'.format(i+1,len(modded_configs),a['experiment_name'],a['experiment_path']))
+            logger.info('Using backend {}'.format(a['backend']))
+            init_backend(a['backend'])
+            graph = Graph(config=c, name='main_graph',logger=logger, global_flags=a, main=True)
+            graph.cacheable = False
+            graph.run()
+            logger.remove(sink_id) 
+    else:
+        if not Path(args['experiment_path']).exists():
+            Path(args['experiment_path']).mkdir(parents=True)
+        sink_id = logger.add(Path(args['experiment_path'],'logs'))
+        logger.info('Using backend {}'.format(args['backend']))
+        init_backend(args['backend'])
+        logger.info('Running experiment {}. Outputs will be saved in {}'.format(args['experiment_name'], args['experiment_path']))
+        graph = Graph(config=config, name='main_graph',logger=logger, global_flags=args, main=True)
+        graph.cacheable = False
+        graph.run()
     shutdown_backend(args['backend'])
 
 if __name__ == '__main__':
